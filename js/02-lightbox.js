42729553bcf758d4966973f2f35fca1fb87584c0
DOPinIT <illyakonor@gmail.com>
2023-01-23 01:16:23 +0200

    hw7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const ulEl = document.querySelector(".gallery");
+
+const images = galleryItems
+  .map(
+    (img) => `<div class="gallery__item">
+  <a class="gallery__link" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      data-source="${img.original}"
+      alt="${img.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+ulEl.insertAdjacentHTML("beforeend", images);
+
+const gallery = new SimpleLightbox(".gallery__item a", {});
